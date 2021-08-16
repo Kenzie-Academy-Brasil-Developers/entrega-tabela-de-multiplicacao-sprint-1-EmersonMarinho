86d8a0da2d1e8e76bcf37da068e7b7c9cedd0e7a
Emerson <emerson_marinhoaz@hotmail.com>
2021-08-15 21:22:15 -0300

    tabela

---
+++ code.js
@@ -0,0 +1,11 @@
+function tabela(n) {
+    let x = [];
+    for (let i = 1; i <= n; i++) {
+        x[i]=[]
+        for(let j = 1; j <= n; j++){
+            x[i][j] = i*j;
+        }
+    }
+    console.table(x)
+}
+tabela(10)
